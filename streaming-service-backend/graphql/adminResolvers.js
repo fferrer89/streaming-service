d8a9c45deb4597093a3c7b41e679203cfa4f82e6
dhanvinpatel <78669309+dhanvinpatel@users.noreply.github.com>
2024-04-27 12:59:51 -0400

    Fixed error message for invalid login

--- streaming-service-backend/graphql/adminResolvers.js
+++ streaming-service-backend/graphql/adminResolvers.js
@@ -56,27 +56,20 @@ export const adminResolvers = {
         const admin = await Admin.findOne({ email: args.email }).select('+password');
 
         if (!admin) {
-          throw new Error('Invalid email or password');
+          throw new GraphQLError('Invalid email or password.');
         }
 
         const isPasswordCorrect = await admin.isPasswordCorrect(args.password, admin.password);
 
         if (!isPasswordCorrect) {
-          throw new Error('Invalid email or password.');
+          throw new GraphQLError('Invalid email or password.');
         }
 
         const token = generateToken(admin._id, 'ADMIN', admin.first_name);
 
         return { admin, token };
       } catch (error) {
-        return {
-          admin: null,
-          token: null,
-          error: {
-            message: 'Error logging in admin',
-            details: error.message
-          }
-        }
+        throw error;
       }
     }
   }
