adffbc84b90df6faadc1be2fa8416f0547ab8b8b
Marco Aurelio Salazar <90633967+marcusats@users.noreply.github.com>
2024-05-03 15:26:22 -0400

    restructure

---
+++ streaming-service-frontend/src/app/login/layout.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { Provider } from 'react-redux';
+import { store } from '@/utils/redux/store';
+import "../globals.css";
+import { ApolloWrapper } from "../ApolloWrapper";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body > 
+      <ApolloWrapper>
+        <Provider store={store}>
+          {children}
+        </Provider>
+        </ApolloWrapper>
+       </body>
+    </html>
+  );
+}
