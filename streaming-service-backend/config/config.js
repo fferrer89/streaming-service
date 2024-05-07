4f50c6d3b8b3c68df9223c8b97e64c865809581d
Marco Aurelio Salazar <90633967+marcusats@users.noreply.github.com>
2024-05-06 20:18:36 -0400

    merging main

--- streaming-service-backend/config/config.js
+++ streaming-service-backend/config/config.js
@@ -1,6 +1,6 @@
 const environment = {
   development: {
-    mongoUrl: 'mongodb://127.0.0.1:27017/streaming-service',
+    mongoUrl: 'mongodb+srv://user554:BHVTeZOx80QQM7jY@cluster0.bui7i1e.mongodb.net/streaming-service',
   },
   production: {
     mongoUrl: 'mongodb://user:pass@example.com:1234/streaming-service',
