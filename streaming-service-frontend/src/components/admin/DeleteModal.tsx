9fd8aad9f8440f8d7a07bb1c9bf3690fc14d060d
dhanvinpatel <78669309+dhanvinpatel@users.noreply.github.com>
2024-05-07 22:31:34 -0400

    Fixed text color

--- streaming-service-frontend/src/components/admin/DeleteModal.tsx
+++ streaming-service-frontend/src/components/admin/DeleteModal.tsx
@@ -14,14 +14,14 @@ const DeleteConfirmationModal: React.FC<ModalProps> = ({ open, item, onClose, ch
     <>
       <div className={`fixed inset-0 flex justify-center items-center
           transition-colors ${open ? 'visible bg-black/20' : 'invisible'}`}>
-        <div onClick={(event) => event.stopPropagation()} className={`bg-white rounded-sm shadow p-6 transition-all
+        <div onClick={(event) => event.stopPropagation()} className={`bg-[#C6AC8E] rounded-sm shadow p-6 transition-all
                         ${open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'}`}>
           <button className='absolute top-2 right-2 p-2' onClick={onClose}>
-            <CgCloseR fontSize={24} className='hover:text-red-500'></CgCloseR>
+            <CgCloseR fontSize={24} className='hover:text-red-700 text-[#22333B]'></CgCloseR>
           </button>
           <div className='flex flex-col justify-center items-center text-center w-60'>
-            <FaTrashAlt fontSize={36} color='red'></FaTrashAlt>
-            <div className='mt-6'>
+            <FaTrashAlt fontSize={36} color='#B91C1C'></FaTrashAlt>
+            <div className='mt-6 text-[#22333B]'>
               <h3 className='text-xl mb-4 font-extrabold'>Confirm Delete</h3>
               <p>Are you sure you want to delete the {item}?</p>
             </div>
